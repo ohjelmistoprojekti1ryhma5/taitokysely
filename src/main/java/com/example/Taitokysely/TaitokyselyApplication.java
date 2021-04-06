964ca4145dac8685b48767ba0cc03e1183ff8e01
Lauri <lauri.vastela@gmail.com>
2021-04-06 15:13:18 +0300

    First commit

---
+++ src/main/java/com/example/Taitokysely/TaitokyselyApplication.java
@@ -0,0 +1,13 @@
+package com.example.Taitokysely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaitokyselyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaitokyselyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Taitokysely/TaitokyselyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Taitokysely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaitokyselyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
